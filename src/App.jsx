aca05414fc0dd8f7bd9c2a03700f1c1e88b42c4d
Agustín Gomez D'Addario <gomezdaddarioagus@gmail.com>
2024-08-05 14:39:42 -0300

    Funciona creacion de usuarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,86 @@
 import React from 'react';
 import './App.css';
-import { Bienvenida } from './components/Bienvenida.jsx';
-import { CollapsibleTable } from './components/TableCollapse.jsx';
+// import { Bienvenida } from './components/Bienvenida.jsx';
+// import { CollapsibleTable } from './components/TableCollapse.jsx';
+import { Amplify } from 'aws-amplify';
+import awsConfig from './aws-exports';
+import { useEffect } from 'react';
+
+Amplify.configure(awsConfig);
+
+import { signUp } from 'aws-amplify/auth';
+
+export async function handleSignUp({ 
+    username,
+    password,
+    email,
+    family_name,
+    address,
+    birthdate,
+    locale,
+    given_name,
+    matricula
+   }) {
+  try {
+    const { isSignUpComplete, userId, nextStep } = await signUp({
+      username,
+      password,
+      options: {
+        userAttributes: {
+          email,
+          family_name,
+          address,
+          birthdate,
+          locale,
+          given_name,
+          'custom:matricula': matricula.toString() // Convierte el número a cadena
+        },
+        // optional
+        autoSignIn: true// or SignInOptions e.g { authFlowType: "USER_SRP_AUTH" }
+      }
+    });
+    console.log(userId, nextStep);
+  } catch (error) {
+    console.log('error signing up:', error);
+  }
+}
+
 
 export default function App() {
+  useEffect(() => {
+    testSignUp();
+  }, []);
 
   return (
       <div className="App">
         <div className='WelcomeTable'>
+{/* |         <SignIn />
           <Bienvenida />
-          <CollapsibleTable />
+          <CollapsibleTable /> */}
         </div>
       </div>
   );
 }
 
+const testUserData = {
+  username: 'testuser1',
+  password: 'testpassword12AC',
+  email: 'gomezdaddarioagus@yahoo.com',
+  family_name: 'Lopez',
+  address: 'Calle Falsa 123',
+  birthdate: '1990-01-01',
+  locale: 'es',
+  given_name: 'Jorge',
+  matricula: 76557890
+};
+
+async function testSignUp() {
+  try {
+    const result = await handleSignUp(testUserData);
+    console.log('Resultado:', result);
+  } catch (error) {
+    console.error('Error en la prueba:', error);
+  }
+}
+
+testSignUp();
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './main.css';
-import App from './App';
+import App from './App.jsx';
+// import { Amplify } from 'aws-amplify';
+// import amplifyconfig from './amplifyconfiguration.json';
+// Amplify.configure(amplifyconfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
