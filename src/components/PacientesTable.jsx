594b930c8faf6bf8ce3a0ea2a6d6fc4c86873b75
Agustín Gomez D'Addario <gomezdaddarioagus@gmail.com>
2024-08-12 11:40:58 -0300

    Cambios de estilos

--- src/components/PacientesTable.jsx
+++ src/components/PacientesTable.jsx
@@ -5,10 +5,12 @@ import CollapsibleTable from './TableCollapse';
 export const PacientesTable = () => {
 
     return (
+        <>
         <div className="container">
             <h1 className="h1">Bienvenido a su<br/> sitio web de Memorium</h1>
             <h2 className="h2">Aquí tiene el historial de sus pacientes</h2>
             <CollapsibleTable />
         </div>
+        </>
     )
 }
