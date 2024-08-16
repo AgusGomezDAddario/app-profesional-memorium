bd9d10efcd6954d7f1eb3c7e6a5e2bd4c1860d72
Agustín Gomez D'Addario <gomezdaddarioagus@gmail.com>
2024-08-16 10:58:55 -0300

    Agrego header

--- src/components/PacientesTable.jsx
+++ src/components/PacientesTable.jsx
@@ -1,18 +1,20 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import "./PacientesTable.css";
 import CollapsibleTable from './TableCollapse';
 import TemporaryDrawer from './material-ui/Drawer';
+import { View } from '@aws-amplify/ui-react';
+import { Header } from './Header';
 
 export const PacientesTable = () => {
-
     return (
-        <>
-            <TemporaryDrawer />
+        <View>
             <div className="container">
+                <Header />
+                <TemporaryDrawer />
                 <h1 className="h1">Bienvenido a su sitio web de Memorium</h1>
                 <h2 className="h2">Aquí tiene el historial de sus pacientes</h2>
                 <CollapsibleTable />
             </div>
-        </>
+        </View >
     )
 }
