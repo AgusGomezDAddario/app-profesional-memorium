18cfde87664a461d95513166cfb9ee598cbc69f1
Agustín Gomez D'Addario <gomezdaddarioagus@gmail.com>
2024-08-18 13:53:54 -0300

    Mejoro seccion de Update

--- src/components/auth/UpdateUserAttributes.jsx
+++ src/components/auth/UpdateUserAttributes.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { updateUserAttribute } from 'aws-amplify/auth';
-import { View } from '@aws-amplify/ui-react';
-import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import Form from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
 import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import './UpdateUserAttributes.css';
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import ListItemText from '@mui/material/ListItemText';
+import { Divider } from '@mui/material';
 
 // async function handleUpdateUserAttribute(attributeKey, value) {
 //   try {
@@ -35,19 +44,44 @@ import TextField from '@mui/material/TextField';
 // }
 
 export const UpdateUser = () => {
-    return(
-        <div className="container">
-        <h1 className="h1">Ingrese la información a modificar</h1>
-        <Box
-            component="form"
-            sx={{
-            '& > :not(style)': { m: 1, width: '25ch' },
-            }}
-            noValidate
-            autoComplete="off"
-        >
-            <TextField id="given_name" label="aaa" variant="outlined" />
-        </Box>
-        </div>
+    return (
+        <Card sx={{ maxWidth: '60em', justifySelf: 'center', margin: '0 auto', marginTop: '2em' }}>
+            <CardHeader sx={{ '& .MuiTypography-root': { fontFamily: 'Gentium Plus, serif' } }}
+                title="Actualizar información de usuario"
+                subheader="Ingrese la información a modificar"
+            />
+            <CardContent>
+                <Form>
+                    <Box>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={6} lg={3}>
+                                <TextField id="given_name" label="Nombre" variant="outlined" />
+                            </Grid>
+                            <Grid item xs={12} sm={6} lg={3}>
+                                <TextField id="family_name" label="Apellido" variant="outlined" />
+                            </Grid>
+                            <Grid item xs={12} sm={6} lg={3}>
+                                <TextField id="custom:matricula" label="Matrícula" variant="outlined" />
+                            </Grid>
+                            <Grid item xs={12} sm={6} lg={3}>
+                                <TextField id="password" label="Contraseña" variant="outlined" />
+                            </Grid>
+                            <Grid item xs={12} sm={6} lg={3}>
+                                <TextField id="confirm_password" label="Repetir Contr." variant="outlined" />
+                            </Grid>
+                        </Grid>
+                        <Divider sx={{paddingTop: '1.3rem'}} />
+                        <Link to='/login' >
+                            <Button sx={{ color: '#2f5496' }}>
+                                <ListItemText primary={'Volver'} sx={{ '& .MuiTypography-root': { fontFamily: 'Gentium Plus, serif' } }} />
+                            </Button>
+                        </Link>
+                        <Button sx={{ color: 'green' }}>
+                            <ListItemText primary={'Enviar'} sx={{ '& .MuiTypography-root': { fontFamily: 'Gentium Plus, serif' } }} />
+                        </Button>
+                    </Box>
+                </Form>
+            </CardContent>
+        </Card>
     )
 }
