50a856280867d7b4936ee0ab86c68ca8e84aab2c
AgusGomezDAddario <gomezdaddarioagus@gmail.com>
2024-08-19 15:46:50 -0300

    Renderizo correctamente el perfil del paciente

--- src/components/ProfilePaciente.jsx
+++ src/components/ProfilePaciente.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 export const ProfilePaciente = () => {
-    const { pacienteId } = useParams();
+    const { id } = useParams();
+    // const [paciente, setPaciente] = useState(null);
+
+
     return (
         <div>
             <h1>ProfilePaciente</h1>
+            <h2>{id}</h2>
         </div>
     )
 }
