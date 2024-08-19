b912acb9a27a2c3238733d2d4276fdb1657d36fd
Agustín Gomez D'Addario <gomezdaddarioagus@gmail.com>
2024-08-19 19:02:42 -0300

    Muestro tabla de resultados en informacion

--- src/components/ProfilePaciente.jsx
+++ src/components/ProfilePaciente.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { usePacientes } from "../pacientesInfo/usePacientes.js";
 import Divider from '@mui/material/Divider';
+import DenseTable from './DenseTable.jsx';
 
 export const ProfilePaciente = () => {
     const { id } = useParams();
@@ -20,12 +21,13 @@ export const ProfilePaciente = () => {
 
     return (
         <div>
-            <p style={{color: 'white', fontSize: '3rem'}}>{pacienteProfile.nombre}</p>
-            <Divider style={{backgroundColor: 'white'}} />
             {pacienteProfile ? (
                 <div>
+                    <p style={{color: 'white', fontSize: '3rem'}}>{pacienteProfile.nombre}</p>
+                    <Divider style={{backgroundColor: 'white'}} />
                     <p style={{color: 'white', fontSize: '1.8rem'}}>Edad: {pacienteProfile.edad}</p>
                     <p style={{color: 'white', fontSize: '1.8rem'}}>Desempeño: {pacienteProfile.desempenoGlobal}%</p>
+                    <DenseTable id={id}/>
                 </div>
             ) : (
                 <p>Cargando...</p>
