4ff913eafb63f5c9bcfefb54d163895a629356c3
AgusGomezDAddario <gomezdaddarioagus@gmail.com>
2024-08-19 16:10:21 -0300

    Muestro informacion del paciente

--- src/components/ProfilePaciente.jsx
+++ src/components/ProfilePaciente.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { usePacientes } from "../pacientesInfo/usePacientes.js";
+import Divider from '@mui/material/Divider';
 
 export const ProfilePaciente = () => {
     const { id } = useParams();
-    // const [paciente, setPaciente] = useState(null);
+    const pacientes = usePacientes();
+    const [pacienteProfile, setPacienteProfile] = useState(null);
 
+    useEffect(() => {
+        pacientes.forEach((paciente) => {
+            if (paciente.id === id) {
+                setPacienteProfile(paciente);
+                console.log(paciente);
+            }
+        });
+    }, [pacientes, id]);
 
     return (
         <div>
-            <h1>ProfilePaciente</h1>
-            <h2>{id}</h2>
+            <p style={{color: 'white', fontSize: '3rem'}}>{pacienteProfile.nombre}</p>
+            <Divider style={{backgroundColor: 'white'}} />
+            {pacienteProfile ? (
+                <div>
+                    <p style={{color: 'white', fontSize: '1.8rem'}}>Edad: {pacienteProfile.edad}</p>
+                    <p style={{color: 'white', fontSize: '1.8rem'}}>Desempeño: {pacienteProfile.desempenoGlobal}%</p>
+                </div>
+            ) : (
+                <p>Cargando...</p>
+            )}
         </div>
-    )
+    );
 }
