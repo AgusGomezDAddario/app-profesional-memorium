8966ed56cfcf9ab33782503d43a76f09b24c1109
Agustín Gomez D'Addario <gomezdaddarioagus@gmail.com>
2024-08-19 20:02:00 -0300

    Retorno a pagina de pacientes

--- src/components/ProfilePaciente.jsx
+++ src/components/ProfilePaciente.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import { usePacientes } from "../pacientesInfo/usePacientes.js";
 import Divider from '@mui/material/Divider';
 import DenseTable from './DenseTable.jsx';
+import { Link } from 'react-router-dom';
 
 export const ProfilePaciente = () => {
     const { id } = useParams();
@@ -26,6 +27,9 @@ export const ProfilePaciente = () => {
                     <Divider style={{backgroundColor: 'white'}} />
                     <p style={{color: 'white', fontSize: '1.8rem', marginTop: '0.3rem'}}>Edad: {pacienteProfile.edad}</p>
                     <p style={{color: 'white', fontSize: '1.8rem', marginTop: '0.3rem'}}>Desempeño: {pacienteProfile.desempenoGlobal}%</p>
+                    <Link to='/login' style={{textDecoration: 'none'}}>
+                        <a style={{color: 'white', fontSize: '1.2rem', marginTop: '0.3rem', marginLeft: '1.5rem', display: 'flex', fontFamily: 'Gentium Plus'}}>Volver a Pacientes</a>
+                    </Link>
                     <DenseTable id={id}/>
                 </div>
             ) : (
