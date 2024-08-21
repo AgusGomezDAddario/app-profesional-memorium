d3fbf65c8ea834b8c162b7d67c9e7ca52fc4920a
AgusGomezDAddario <gomezdaddarioagus@gmail.com>
2024-08-21 18:25:08 -0300

    Proveeo de context de anotaciones

--- src/components/PacientesAnotaciones.jsx
+++ src/components/PacientesAnotaciones.jsx
@@ -1,25 +1,49 @@
 import React from "react";
 import { OutlinedCard } from "./material-ui/CardAnotaciones.jsx";
 import { TextFieldAnotacion } from "./material-ui/TextFieldAnotaciones.jsx";
-
+import { AnotacionContext } from "../contexts/anotaciones";
+import { Button } from "@mui/material";
+import { useContext } from "react";
 
 const PacientesAnotaciones = () => {
+  const { anotacionFinal, clearAnotacion } = useContext(AnotacionContext);
+
+  const imprimirAnotacion = () => {
+    console.log(anotacionFinal);
+    clearAnotacion();
+  };
+
   return (
+    <div
+      style={{
+        marginTop: "1.5rem",
+        width: "100%",
+        alignContent: "center",
+        justifyContent: "center",
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
+        gap: "1rem",
+      }}
+    >
       <div
         style={{
-          marginTop: "1.5rem",
-          marginX: "auto",
-          width: "100%",
-          alignContent: "center",
+          display: "flex",
+          flexDirection: "column",
           justifyContent: "center",
-          display: "grid",
-          gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
-          gap: "1rem",
+          alignItems: "stretch",
         }}
       >
-          <TextFieldAnotacion />
-        <OutlinedCard />
+        <TextFieldAnotacion />
+        <Button
+          variant="text"
+          sx={{ color: "white" }}
+          onClick={imprimirAnotacion}
+        >
+          Enviar
+        </Button>
       </div>
+      <OutlinedCard />
+    </div>
   );
 };
 
