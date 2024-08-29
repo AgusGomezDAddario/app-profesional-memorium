ada8bc07bec476105301f496d160050e141a6714
Agustín Gomez D'Addario <gomezdaddarioagus@gmail.com>
2024-08-29 13:47:02 -0300

    Agrego icono de microfono

--- src/components/PacientesTable.jsx
+++ src/components/PacientesTable.jsx
@@ -4,6 +4,7 @@ import CollapsibleTable from './TableCollapse';
 import TemporaryDrawer from './material-ui/Drawer';
 import { View } from '@aws-amplify/ui-react';
 import { Header } from './Header';
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
 
 export const PacientesTable = () => {
     return (
@@ -11,8 +12,13 @@ export const PacientesTable = () => {
             <div className="container">
                 <Header />
                 <TemporaryDrawer />
-                <h1 className="h1">Bienvenido a su sitio web de Memorium</h1>
-                <h2 className="h2">Aquí tiene el historial de sus pacientes</h2>
+                <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                    <div style={{display: 'block'}}>
+                        <h1 className="h1">Bienvenido a su sitio web de Memorium</h1>
+                        <h2 className="h2">Aquí tiene el historial de sus pacientes</h2>
+                    </div>
+                    <VolumeUpIcon style={{paddingRight: '2rem', fontSize: '5rem', color: 'white', paddingTop: '0'}}/>
+                </div>
                 <CollapsibleTable />
             </div>
         </View >
