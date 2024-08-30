44917239d8adad30f89ddfe4e8f6ee1645480369
Agustín Gomez D'Addario <gomezdaddarioagus@gmail.com>
2024-08-30 16:44:15 -0300

    Agrego carrousel sin fotos

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,6 +2,12 @@ import './HomePage.css'
 import logo from '../images/logo_memorium_white.png'
 import image from '../images/image_brain.png'
 import OutlinedCard from './material-ui/Card'
+import EmblaCarousel from './home_carusel/js/EmblaCarousel'
+import './home_carusel/css/embla.css'
+
+const OPTIONS = {}
+const SLIDE_COUNT = 3
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 
 export const HomePage = () => {
 
@@ -15,6 +21,7 @@ export const HomePage = () => {
         <h3 className='welcome-text'>Explora nuestras herramientas diseñadas para apoyar tu práctica clínica y facilitar el seguimiento de tratamientos</h3>
         <img src={image} className="imagen_central" alt="Imagen de cerebro con un celular" />
         <h2 className='welcome-text'>Descubra el universo Memorium y la variedad de herramientas que ofrecemos para el tratamiento en la pérdida de la memoria de trabajo</h2>
+        <EmblaCarousel slides={SLIDES} options={OPTIONS} />
     </>
   )
 }
