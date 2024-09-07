74e79b4d16f8a7bf607765e9e805316a48b49d5e
Agustín Gomez D'Addario <gomezdaddarioagus@gmail.com>
2024-09-07 15:20:04 -0300

    Agrego link a home

--- src/components/ConozcaLaPlataforma.jsx
+++ src/components/ConozcaLaPlataforma.jsx
@@ -3,6 +3,9 @@ import "./ConozcaLaPlataforma.css";
 import Divider from "@mui/material/Divider";
 import EmblaCarousel from './platform_carusel/js/EmblaCarousel';
 import './platform_carusel/css/embla.css';
+import image from '../images/image_brain.png';
+import { Link } from "react-router-dom";
+
 
 const OPTIONS = {}
 const SLIDE_COUNT = 8
@@ -17,7 +20,22 @@ export const ConozcaLaPlataforma = () => {
         Memorium
       </h2>
       <Divider style={{ backgroundColor: "white" }} />
+      <Link to="/" style={{ textDecoration: "none" }}>
+            <a
+              style={{
+                color: "white",
+                fontSize: "1.2rem",
+                marginTop: "0.3rem",
+                marginLeft: "1.5rem",
+                display: "flex",
+                fontFamily: "Gentium Plus",
+              }}
+            >
+              Volver a Home Page
+            </a>
+          </Link>
       <EmblaCarousel slides={SLIDES} options={OPTIONS}/>
+      <img src={image} className="imagen_central" alt="Imagen de cerebro con un celular" />
     </div>
   );
 };
