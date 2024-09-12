ffda794348fcd40a688da0cd209f7593df55b6c4
Agustín Gomez D'Addario <gomezdaddarioagus@gmail.com>
2024-09-12 16:02:19 -0300

    Finalizo carusel

--- src/components/platform_carusel/images_carrusel.js
+++ src/components/platform_carusel/images_carrusel.js
@@ -2,47 +2,41 @@ export const images_carrusel = [
     {
         id: 0,
         src: '../../src/images/platform_1.png',
-        text: 'Conozca nuestra plataforma: En Memorium podrás estar al día con los tratamientos de tus pacientes y conocer los juegos que tenemos disponibles para ellos.',
+        text: 'Bienvenido a Memorium. Esta es nuestra plataforma diseñada para profesionales de la salud que buscan monitorear y mejorar la memoria de trabajo de sus pacientes. Aquí, podrás gestionar tratamientos y explorar los juegos especializados que hemos creado para ayudar a tus pacientes a mejorar su memoria.',
     },
     {
         id: 1,
         src: '../../src/images/platform_3.png',
-        text: 'En el menú inicial, podrás acceder a cualquiera de las tres secciones que tenemos en Memorium: "Mis pacientes", "Conocer Plataforma" o "Juegos disponibles".',
+        text: 'Esta es la pantalla principal de Memorium. Desde aquí puedes navegar fácilmente entre tres secciones clave: `Mis pacientes` para gestionar la información de tus pacientes, `Conocer Plataforma` para familiarizarte con las herramientas disponibles, y `Juegos disponibles` para conocer los juegos diseñados para entrenar la memoria.',
     },
     {
         id: 2,
         src: '../../src/images/platform_2.png',
-        text: 'Si accedes a la gestión de pacientes, podrás iniciar sesión con tu cuenta de Memorium, o crear una nueva cuenta si aún no tienes una.',
+        text: 'Para gestionar a tus pacientes, primero necesitas iniciar sesión en Memorium. Si ya tienes una cuenta, simplemente introduce tus credenciales. Si aún no estás registrado, puedes crear una cuenta rápidamente para comenzar a usar nuestras herramientas.',
     },
     {
         id: 3,
         src: '../../src/images/platform_4.png',
-        text: 'En la sección "Mis pacientes", podrás ver a los pacientes que tienes asignados, y acceder a su información personal, así como los resultados en los juegos.',
+        text: 'En la sección `Mis pacientes` podrás ver una lista completa de todos los pacientes asignados a tu cuidado. Aquí podrás revisar su información personal, como nombre, edad, y también ver sus resultados en los diferentes juegos de memoria.',
     },
     {
         id: 4,
         src: '../../src/images/platform_5.png',
-        text: 'Si desplegas la información de cada paciente, verás los resultados más específicos y acceder a la pantalla específica de ese paciente.',
+        text: 'Al hacer clic en un paciente, podrás ver detalles específicos sobre su rendimiento en los juegos de memoria. Esto incluye datos como los puntajes en cada juego y un historial completo de su progreso. Esta información te permitirá evaluar de manera detallada su evolución.',
     },
     {
         id: 5,
         src: '../../src/images/platform_6.png',
-        text: 'En la columna de Clasificación podés asignarle estados como: "Mejorando", "En tratamiento", "Empeorando", "Consulta", "En Alta".',
+        text: 'Memorium te permite asignar estados específicos a cada paciente para que puedas tener una visión clara de su progreso. Las clasificaciones incluyen `Mejorando`, `En tratamiento`, `Empeorando`, `Consulta`, y `En alta`. Esto te ayudará a organizar y priorizar la atención que brindas.',
     },
     {
         id: 6,
         src: '../../src/images/platform_7.png',
-        text: 'Para poder facilitarte la búsqueda de pacientes, podés filtralos por el estado asignado o verlos en orden por su nombre o edad.',
+        text: 'Para hacer tu trabajo más eficiente, puedes filtrar a tus pacientes según el estado de clasificación que les hayas asignado. También puedes ordenarlos por nombre o edad, lo que te permitirá encontrar rápidamente la información que necesitas.',
     },
     {
         id: 7,
         src: '../../src/images/platform_8.png',
-        text: 'Aquí hay gráficos de los rendimientos del paciente y podés generar anotaciones sobre lo que creas conveniente.',
+        text: 'En esta pantalla, verás gráficos detallados que muestran el rendimiento de tus pacientes en los juegos de memoria. Además, puedes agregar anotaciones sobre sus progresos o cualquier observación relevante, lo que te permitirá hacer un seguimiento más completo de su evolución.',
     },
-    {
-        id: 8,
-        src: '../../src/images/platform_9.png',
-        text: 'Además, en la sección de juegos, podés conocer de qué manera tus pacientes pueden llevar a cabo sus tratamientos y además, el trasfondo teórico de cada juego.',
-    },
-
 ]
--- src/components/platform_carusel/js/EmblaCarousel.jsx
+++ src/components/platform_carusel/js/EmblaCarousel.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import '../images_carrusel'
+import React from 'react';
+import '../images_carrusel';
 import {
   PrevButton,
   NextButton,
   usePrevNextButtons
-} from './EmblaCarouselArrowButtons'
-import useEmblaCarousel from 'embla-carousel-react'
-import { images_carrusel } from '../images_carrusel'
+} from './EmblaCarouselArrowButtons';
+import useEmblaCarousel from 'embla-carousel-react';
+import { images_carrusel } from '../images_carrusel';
 import AudioPlayer from '../../AudioInstrucciones';
 
-
 const EmblaCarousel = (props) => {
-  const { slides, options } = props
-  const [emblaRef, emblaApi] = useEmblaCarousel(options)
+  const { slides, options } = props;
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
 
   const {
     prevBtnDisabled,
     nextBtnDisabled,
     onPrevButtonClick,
     onNextButtonClick
-  } = usePrevNextButtons(emblaApi)
+  } = usePrevNextButtons(emblaApi);
 
   return (
     <section className="embla">
@@ -27,13 +26,17 @@ const EmblaCarousel = (props) => {
         <div className="embla__container">
           {slides.map((index) => (
             <div className="embla__slide embla__class-name" key={index}>
-                <img 
+              <img 
                 className="embla__slide__img"
                 src={images_carrusel[index].src} 
-                alt="Imagen de interfaz de Memorium App Profesional" />
+                alt="Imagen de interfaz de Memorium App Profesional" 
+              />
               <div>
                 <p className='embla__slide__text'>{images_carrusel[index].text}</p>
               </div>
+              <div className="embla__audio">
+                <AudioPlayer audioSrc={`https://mybuckets3appprofesionalesmemorium.s3.us-east-2.amazonaws.com/${index}.mp3`} />
+            </div>
             </div>
           ))}
         </div>
@@ -44,12 +47,9 @@ const EmblaCarousel = (props) => {
           <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
           <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
         </div>
-        <div className="embla__audio">
-          <AudioPlayer audioSrc="https://mybuckets3appprofesionalesmemorium.s3.us-east-2.amazonaws.com/instructivo_home_pacientes.mp3" />
-        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default EmblaCarousel
+export default EmblaCarousel;
