122e920642e61961698277210461ace2981db479
Agustín Gomez D'Addario <gomezdaddarioagus@gmail.com>
2024-09-13 12:50:08 -0300

    Cambio de chart

--- src/components/material-ui/Chart.jsx
+++ src/components/material-ui/Chart.jsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
 import { BarChart } from "@mui/x-charts/BarChart";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
@@ -6,6 +8,8 @@ import { usePacientes } from "../../pacientesInfo/usePacientes.js";
 import "./Chart.css";
 
 export const SimpleCharts = () => {
+  const [itemData, setItemData] = useState();
+  const [axisData, setAxisData] = useState();
   const { id } = useParams();
   const pacientes = usePacientes();
   const [pacienteProfile, setPacienteProfile] = useState(null);
@@ -21,57 +25,67 @@ export const SimpleCharts = () => {
     return <div>Loading...</div>;
   }
 
-  return (
-    <div className="chart-container">
-      <BarChart
-        xAxis={[
-          {
-            id: "desempeño_aciertos",
-            data: ["Juego 1", "Juego 2", "Juego 3", "Juego 4", "Juego 5"],
-            scaleType: "band",
-          },
-        ]}
-        series={[
-          {
-            data: [
-              pacienteProfile.historial[0].aciertos,
-              pacienteProfile.historial[1].aciertos,
-              pacienteProfile.historial[2].aciertos,
-              pacienteProfile.historial[3].aciertos,
-              pacienteProfile.historial[4].aciertos,
-            ],
-          },
-        ]}
-        width={700}
-        height={500}
-        colors={["#00FF00"]}
-        barLabel="value"
-      />
+  const barChartsParams = {
+    series: [
+      {
+        id: "aciertos",
+        data: [
+          pacienteProfile.historial[0].aciertos,
+          pacienteProfile.historial[1].aciertos,
+          pacienteProfile.historial[2].aciertos,
+          pacienteProfile.historial[3].aciertos,
+          pacienteProfile.historial[4].aciertos,
+        ],
+        label: "Aciertos",
+        stack: "total",
+        color: "#1ce418",
+        highlightScope: {
+          highlight: "item",
+        },
+      },
+      {
+        id: "errores",
+        data: [
+          pacienteProfile.historial[0].errores,
+          pacienteProfile.historial[1].errores,
+          pacienteProfile.historial[2].errores,
+          pacienteProfile.historial[3].errores,
+          pacienteProfile.historial[4].errores,
+        ],
+        label: "Errores",
+        stack: "total",
+        color: "#e72f2f",
+        highlightScope: {
+          highlight: "item",
+        },
+      },
+    ],
+    xAxis: [
+      {
+        data: ["Juego 1", "Juego 2", "Juego 3", "Juego 4", "Juego 5"],
+        scaleType: "band",
+        id: "axis1",
+      },
+    ],
+    height: 500,
+    width: 700,
+  };
 
-      <BarChart
-        xAxis={[
-          {
-            id: "desempeño_errores",
-            data: ["Juego 1", "Juego 2", "Juego 3", "Juego 4", "Juego 5"],
-            scaleType: "band",
-          },
-        ]}
-        series={[
-          {
-            data: [
-              pacienteProfile.historial[0].errores,
-              pacienteProfile.historial[1].errores,
-              pacienteProfile.historial[2].errores,
-              pacienteProfile.historial[3].errores,
-              pacienteProfile.historial[4].errores,
-            ],
-          },
-        ]}
-        width={700}
-        height={500}
-        colors={["#FF0000"]}
-        barLabel="value"
-      />
-    </div>
+  return (
+    <Stack
+      direction={{ xs: "column", md: "row" }}
+      spacing={{ xs: 0, md: 4 }}
+      sx={{ width: "100%" }}
+    >
+      <Box sx={{ flexGrow: 1 }}>
+        <div className="chart-container">
+          <BarChart
+            {...barChartsParams}
+            onItemClick={(event, d) => setItemData(d)}
+            onAxisClick={(event, d) => setAxisData(d)}
+          />
+        </div>
+      </Box>
+    </Stack>
   );
 };
