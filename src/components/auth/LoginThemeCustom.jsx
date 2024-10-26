94d33dda1efde3a3d00b045f47e076308ca311f9
Agustín Gomez D'Addario <gomezdaddarioagus@gmail.com>
2024-10-26 19:38:50 -0300

    Agrego nuevos detalles

--- src/components/auth/LoginThemeCustom.jsx
+++ src/components/auth/LoginThemeCustom.jsx
@@ -1,24 +1,19 @@
-import './Login.css';
-import React from 'react';
-import { LoginSystem } from './login.jsx';
-
-import {
-  ThemeProvider,
-  useTheme,
-  View,
-} from '@aws-amplify/ui-react';
+import "./Login.css";
+import React from "react";
+import { LoginSystem } from "./login.jsx";
+import { ThemeProvider, useTheme, View } from "@aws-amplify/ui-react";
+import { Link } from "react-router-dom";
 
 export const AuthenticatorTheme = () => {
   const { tokens } = useTheme();
   const theme = {
-    name: 'Auth Theme',
+    name: "Auth Theme",
     tokens: {
-      components:
-      {
+      components: {
         authenticator: {
           router: {
-            boxShadow: `0 0 16px ${tokens.colors.overlay['10']}`,
-            borderWidth: '0',
+            boxShadow: `0 0 16px ${tokens.colors.overlay["10"]}`,
+            borderWidth: "0",
           },
           form: {
             padding: `${tokens.space.medium} ${tokens.space.xl} ${tokens.space.medium}`,
@@ -26,10 +21,10 @@ export const AuthenticatorTheme = () => {
         },
         button: {
           primary: {
-            backgroundColor: '#2f5496', // Replace with your blue color
+            backgroundColor: "#2f5496", // Replace with your blue color
           },
           link: {
-            color: tokens.colors.neutral['80'],
+            color: tokens.colors.neutral["80"],
           },
         },
         fieldcontrol: {
@@ -39,10 +34,10 @@ export const AuthenticatorTheme = () => {
         },
         tabs: {
           item: {
-            color: tokens.colors.neutral['80'],
+            color: tokens.colors.neutral["80"],
             _active: {
-              borderColor: tokens.colors.neutral['100'],
-              color: '#2f5496', // Replace with your blue color
+              borderColor: tokens.colors.neutral["100"],
+              color: "#2f5496", // Replace with your blue color
             },
           },
         },
@@ -51,10 +46,24 @@ export const AuthenticatorTheme = () => {
   };
 
   return (
-    <View style={{textAlign: 'justify'}}>
+    <View style={{ textAlign: "justify"}}>
       <ThemeProvider theme={theme}>
         <LoginSystem />
       </ThemeProvider>
+      <View style={{ textAlign: "center"}}>
+      <Link to="/" style={{ textDecoration: "none" }}>
+          <a
+            style={{
+              color: "white",
+              fontSize: "1.3rem",
+              fontFamily: "Gentium Plus",
+              marginTop: "2rem",
+            }}
+          >
+            Volver a Inicio
+          </a>
+        </Link>
+      </View>
     </View>
   );
-}
+};
